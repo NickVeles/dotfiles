7552de827b8aa72cbe4f3ef3f7e0b3dee2011ba7
Veles <81438101+NickVeles@users.noreply.github.com>
2024-05-23 16:22:50 +0200

    Added a listener

---
+++ .config/hypr/scripts/listener.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# listener.sh - A script for handling Hyprland event changes.
+
+fetch_all_workspaces() {
+  # fetch the IDs of all workspaces:
+  # - that are not secret (id < 0)
+  # - in the JSON format, e.g. [1, 2, 3]
+  hyprctl workspaces -j | jq -c '[.[] | select(.id > 0) | .id]'
+}
+
+fetch_active_workspace() {
+  # fetch the ID of the active workspace
+  hyprctl activeworkspace -j | jq '.id'
+}
+
+handle() {
+  case $1 in
+    workspace*)
+      # Status Bar - all workspaces
+      eww update workspaces="$(fetch_all_workspaces)" &&
+      # Status Bar - active workspace
+      eww update acSpace="$(fetch_active_workspace)";;
+  esac
+}
+
+socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
