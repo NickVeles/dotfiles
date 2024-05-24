1ca1e88b3d3b3bebd1cc81210f82a1c2f30309ae
Veles <81438101+NickVeles@users.noreply.github.com>
2024-05-24 13:36:34 +0200

    Fixed handleAudio.sh bug

--- .config/eww/scripts/handleAudio.sh
+++ .config/eww/scripts/handleAudio.sh
@@ -2,16 +2,18 @@
 
 # handleAudio.sh - handles volume and updates the corresponding stream icon in eww
 
+sleep 1
+
 # Run the scripts at least once
-exec $HOME/.config/eww/scripts/updateOut.sh
-exec $HOME/.config/eww/scripts/updateMic.sh
+$HOME/.config/eww/scripts/updateMic.sh
+$HOME/.config/eww/scripts/updateOut.sh
 
 # Subscribe to PulseAudio events
 pactl subscribe | while read -r event; do
     if echo "$event" | grep -q "change"; then
 
         # When a change event occurs:
-        ./updateOut.sh
-        ./updateMic.sh
+        $HOME/.config/eww/scripts/updateMic.sh
+        $HOME/.config/eww/scripts/updateOut.sh
     fi
 done
