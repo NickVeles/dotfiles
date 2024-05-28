ba5a4b65fd036c36d18518c1bbdc28d4c5071f95
Veles <81438101+NickVeles@users.noreply.github.com>
2024-05-28 18:57:25 +0200

    Add files via upload

---
+++ .config/hypr/scripts/screenshot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Define the directory path
+target=$1
+directory="$HOME/Pictures/Screenshots/$(date +'%Y_%m')"
+file="$directory/$(date +'%d_%H%M%S').png"
+
+# Check if the directory exists
+if [ -d "$directory" ]; then
+    # Directory exists, prtsc to the directory
+    grimblast copysave "$target" "$file"
+else
+    # Directory doesn't exist, create the directory and prtsc to it
+    mkdir -p "$directory"
+    grimblast copysave "$target" "$file"
+fi
