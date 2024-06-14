5cc0b22d0dc39cffd6c8ae23ad896d3046cdbc43
Veles <81438101+NickVeles@users.noreply.github.com>
2024-06-14 16:57:10 +0200

    Add swappy

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -2,15 +2,25 @@
 
 # Define the directory path
 target=$1
+mode=$2
 directory="$HOME/Pictures/Screenshots/$(date +'%Y_%m')"
 file="$directory/$(date +'%d_%H%M%S').png"
+
+# Check edit mode
+handle_mode() {
+    if [ "$mode" == "edit" ]; then
+        swappy -f $(grimblast copysave "$target" "$file") -o "$file"
+    else
+        grimblast copysave "$target" "$file"
+    fi 
+}
 
 # Check if the directory exists
 if [ -d "$directory" ]; then
     # Directory exists, prtsc to the directory
-    grimblast copysave "$target" "$file"
+    handle_mode
 else
     # Directory doesn't exist, create the directory and prtsc to it
     mkdir -p "$directory"
-    grimblast copysave "$target" "$file"
+    handle_mode
 fi
