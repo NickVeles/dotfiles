700c3f8e0d38f453af0ed03cba776c28de711dbd
Veles <81438101+NickVeles@users.noreply.github.com>
2024-06-29 12:58:12 +0200

    Update micToggle.sh
    - automatically detects the microphone
    - (un)mutes all microphones

--- .config/hypr/scripts/micToggle.sh
+++ .config/hypr/scripts/micToggle.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-microphone="alsa_input.pci-0000_07_00.4.analog-stereo.4"
-# Toggle the source
-pamixer --toggle-mute --source "$microphone"
+# List all input sources
+input_sources=$(pamixer --list-sources | grep input | awk '{print $2}' | sed 's/"//g')
 
-# Check if the source is muted
-if [ $(pamixer --get-mute --source "$microphone") = "true" ]; then
-    # Play mute sound
-    paplay $HOME/.sounds/mute.mp3 --volume 36000
+# Variable to store the final mute status
+final_status=""
+
+# Iterate over each input source
+for source in $input_sources; do
+    # Toggle the mute status for each input source
+    pamixer --toggle-mute --source "$source"
+
+    # Get the mute status after toggling
+    mute_status=$(pamixer --get-mute --source "$source")
+
+    # Store the final status for sound notification
+    final_status=$mute_status
+done
+
+# Play the corresponding sound based on the final status
+if [ "$final_status" = "true" ]; then
+    paplay ~/.sounds/mute.mp3 --volume 36000
 else
-    # Play unmute sound
-    paplay $HOME/.sounds/unmute.mp3 --volume 36000
+    paplay ~/.sounds/unmute.mp3 --volume 36000
 fi
